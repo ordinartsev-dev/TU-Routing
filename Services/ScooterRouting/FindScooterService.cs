25461ec4724eac25f0865c5aaa26a51a8689f3a0
Daniil <45737205+GMSDaniil@users.noreply.github.com>
2025-08-15 23:29:35 +0200

    Update FindScooterService.cs

--- Services/ScooterRouting/FindScooterService.cs
+++ Services/ScooterRouting/FindScooterService.cs
@@ -21,7 +21,7 @@ namespace Backend.Services
         {
             try
             {
-                string url = $"https://dev.cherep.co/tubify/api/bikes/nearby?coords={Lat},{Lon}&radius=500&limit=10";
+                string url = $"http://tubify-external-api:8000/api/bikes/nearby?coords={Lat},{Lon}&radius=500&limit=10";
             
                 HttpResponseMessage response = await _httpClient.GetAsync(url);
                 response.EnsureSuccessStatusCode();
@@ -69,3 +69,4 @@ namespace Backend.Services
         }
     }
 }
+
